12860467cd2ede6252c87878a0d8d84898461723
Samuel Tauil <samueltauil@gmail.com>
2018-06-04 08:58:19 -0400

    initial commit

---
+++ sparknaivebayes/src/main/java/com/redhat/ml/NaiveBayesExample.java
@@ -0,0 +1,59 @@
+package com.redhat.ml;
+
+import org.apache.spark.ml.classification.NaiveBayes;
+import org.apache.spark.ml.classification.NaiveBayesModel;
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by samueltauil on 5/31/18.
+ */
+public class NaiveBayesExample {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("NaiveBayesExample")
+                .master("local")
+                .getOrCreate();
+
+        spark.sparkContext().setLogLevel("ERROR");
+
+
+        // $example on$
+        // Load training data
+        Dataset<Row> dataFrame =
+                spark.read().format("libsvm").load("src/main/resources/iris_svm.txt");
+        // Split the data into train and test
+        Dataset<Row>[] splits = dataFrame.randomSplit(new double[]{0.6, 0.4}, 1234L);
+        Dataset<Row> train = splits[0];
+        Dataset<Row> test = splits[1];
+
+        // create the trainer and set its parameters
+        NaiveBayes nb = new NaiveBayes();
+
+        // train the model
+        NaiveBayesModel model = nb.fit(train);
+
+        // Select example rows to display.
+        Dataset<Row> predictions = model.transform(test);
+        predictions.show(200);
+
+        // compute accuracy on the test set
+        MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+                .setLabelCol("label")
+                .setPredictionCol("prediction")
+                .setMetricName("accuracy");
+        double accuracy = evaluator.evaluate(predictions);
+        accuracy *= 100;
+        DecimalFormat df = new DecimalFormat("0.000");
+
+        System.out.println("Test set accuracy = " + df.format(accuracy) + "%");
+
+
+        spark.stop();
+    }
+}
